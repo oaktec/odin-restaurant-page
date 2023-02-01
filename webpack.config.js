cbb81512c6677243d393f8500fa1d30fb795e115
oaktec <54672803+oaktec@users.noreply.github.com>
2023-02-01 16:53:41 +0000

    Clean dist run

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: {
     index: "./src/index.js",
   },
@@ -17,6 +16,7 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
